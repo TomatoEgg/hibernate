ea3301bc301ce16b87d178aad8dee7ab62e8d9ee
ejunxie <jun.xie@ericsson.com>
2014-11-20 21:45:02 +0100

    able to write and read a user from DB. Also proper cleanup at the end

--- hb/src/main/java/com/jx/hibernate/HibernateSessionManager.java
+++ hb/src/main/java/com/jx/hibernate/HibernateSessionManager.java
@@ -1,25 +1,38 @@
 package com.jx.hibernate;
  
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
  
 public class HibernateSessionManager {
-  private static final SessionFactory sessionFactory = buildSessionFactory();
- 
-  private static SessionFactory buildSessionFactory() 
+  private static SessionFactory sessionFactory = createSessionFactory();
+  private static ServiceRegistry serviceRegistry;
+
+  public static SessionFactory createSessionFactory() 
   {
-    try 
-    {
-	  // Create the SessionFactory from hibernate.cfg.xml
-	  return new Configuration().configure().buildSessionFactory();
-	} 
-    catch (Throwable ex) 
-    {
-	  // Make sure you log the exception, as it might be swallowed
-	  System.err.println("SessionFactory creation failed." + ex);
-	  throw new ExceptionInInitializerError(ex);
-    }
+     Configuration configuration = new Configuration();
+     configuration.configure();
+     serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+     sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+     return sessionFactory;
   }
+  
+  //This is deprecated
+//  private static SessionFactory buildSessionFactory() 
+//  {
+//    try 
+//    {
+//	  // Create the SessionFactory from hibernate.cfg.xml
+//	  return new Configuration().configure().buildSessionFactory();
+//	} 
+//    catch (Throwable ex) 
+//    {
+//	  // Make sure you log the exception, as it might be swallowed
+//	  System.err.println("SessionFactory creation failed." + ex);
+//	  throw new ExceptionInInitializerError(ex);
+//    }
+//  }
  
   public static SessionFactory getSessionFactory() 
   {
@@ -29,6 +42,6 @@ public class HibernateSessionManager {
   public static void shutdown() 
   {
     // Close caches and connection pools
-	 getSessionFactory().close();
+    sessionFactory.close();
   }
 }
--- hb/src/main/java/com/jx/hibernate/Run.java
+++ hb/src/main/java/com/jx/hibernate/Run.java
@@ -1,26 +1,64 @@
 package com.jx.hibernate;
  
 import java.util.Date;
+import java.util.List;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
  
 public class Run {
- 
-/**
- * @param args
- */
- public static void main(String[] args) {
- Session session = HibernateSessionManager.getSessionFactory().openSession();
- 
- session.beginTransaction();
- User user = new User();
- 
- user.setUserId(1);
- user.setUsername("James");
- user.setCreatedBy("Application");
- user.setCreatedDate(new Date());
- 
- session.save(user);
- session.getTransaction().commit();
-}
- 
+  public static void main(String[] args) {
+    Session session = HibernateSessionManager.getSessionFactory().openSession();
+//    Session session = HibernateSessionManager.getSessionFactory().getCurrentSession();
+    Transaction t = null;
+   
+    //Write some users into the table
+    try
+    {
+      t = session.beginTransaction();
+      User user = new User();
+      
+      user.setUserId(1);
+      user.setUsername("James");
+      user.setCreatedBy("Application");
+      user.setCreatedDate(new Date());
+      
+      session.save(user);
+      t.commit();
+    }
+    catch (HibernateException e)
+    {
+      if (null != t)
+      {
+        t.rollback();
+      }
+      e.printStackTrace();
+    }
+//    finally
+//    {
+//      session.close();
+//    }
+    
+    System.out.println("Users are added to database");
+    //Read all users
+//    session = HibernateSessionManager.getSessionFactory().openSession();
+    try {
+      t = session.beginTransaction();
+      //Using HQL (Hibernate Query Language) to read data from the database
+      List<User> userList = session.createQuery("from User").list();
+      for (User u : userList)
+      {
+        System.out.println(u.toString());
+      }
+      t.commit();
+    } catch (HibernateException e) {
+      e.printStackTrace();
+    }
+    finally {
+      session.close();
+      HibernateSessionManager.shutdown();
+      System.out.println("Shut down");
+    }
+  }
 }
