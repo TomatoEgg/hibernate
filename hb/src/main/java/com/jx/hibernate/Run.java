b988cc52370a616f156aa563bff8148a8351489d
ejunxie <jun.xie@ericsson.com>
2014-11-22 14:49:59 +0100

    added one-to-one mapping example

--- hb/src/main/java/com/jx/hibernate/Run.java
+++ hb/src/main/java/com/jx/hibernate/Run.java
@@ -6,11 +6,85 @@ import java.util.List;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import com.jx.hibernate.onetoone.Employee;
+import com.jx.hibernate.onetoone.EmployeeDetail;
  
 public class Run {
   public static void main(String[] args) {
+    WriteAndReadOneUser();
+    OneToOneMapping();
+    cleanup();
+  }
+
+  private static void cleanup() {
+    HibernateSessionManager.shutdown();
+  }
+
+  private static void OneToOneMapping() {
+    System.out.println("Hibernate One-To-One example");
+    
     Session session = HibernateSessionManager.getSessionFactory().openSession();
-//    Session session = HibernateSessionManager.getSessionFactory().getCurrentSession();
+    session.beginTransaction();
+
+    EmployeeDetail employeeDetail = new EmployeeDetail("10th Street", "LA", "San Francisco", "U.S.");
+    Employee employee = new Employee("Nina", "Mayers", new java.sql.Date(121212), "114-857-965");
+    employee.setEmployeeDetail(employeeDetail);
+    employeeDetail.setEmployee(employee);
+    session.save(employee);
+    
+    employeeDetail = new EmployeeDetail("9th Street", "LA", "San Francisco", "U.S.");
+    employee = new Employee("Sofia", "Xie", new java.sql.Date(141414), "0731245789");
+    employee.setEmployeeDetail(employeeDetail);
+    employeeDetail.setEmployee(employee);
+    session.save(employee);
+
+    {
+      System.out.println("results after adding employees");
+      List<Employee> employees = session.createQuery("from Employee").list();
+      for (Employee employee1 : employees) {
+        System.out.println(employee1.getFirstname() + " , " + 
+            employee1.getLastname() + ", " + 
+            employee1.getEmployeeDetail().getState());
+      }
+      List<EmployeeDetail> details = session.createQuery("from EmployeeDetail").list();
+      for (EmployeeDetail ed : details)
+      {
+        System.out.println(ed.getEmployeeId() + " , " + ed.getCountry() + ", " + 
+            ed.getState() + " , " + ed.getCity() + " , " + ed.getStreet());
+      }
+    }
+
+    session.getTransaction().commit();
+    
+    //Now let's delete the employee 2 from employee table, since cascading is enabled, we expect the
+    //corresponding row in the employeedetail tabel to be removed too.
+    session.beginTransaction();
+    session.delete(employee);
+    session.getTransaction().commit();
+    
+    {
+      System.out.println("results after deleting the last employee");
+      List<Employee> employees = session.createQuery("from Employee").list();
+      for (Employee employee1 : employees) {
+        System.out.println(employee1.getFirstname() + " , " + 
+            employee1.getLastname() + ", " + 
+            employee1.getEmployeeDetail().getState());
+      }
+      List<EmployeeDetail> details = session.createQuery("from EmployeeDetail").list();
+      for (EmployeeDetail ed : details)
+      {
+        System.out.println(ed.getEmployeeId() + " , " + ed.getCountry() + ", " + 
+            ed.getState() + " , " + ed.getCity() + " , " + ed.getStreet());
+      }
+    }
+    
+    session.close();
+  }
+
+  private static void WriteAndReadOneUser() {
+    Session session = HibernateSessionManager.getSessionFactory().openSession();
+  //  Session session = HibernateSessionManager.getSessionFactory().getCurrentSession();
     Transaction t = null;
    
     //Write some users into the table
@@ -35,14 +109,15 @@ public class Run {
       }
       e.printStackTrace();
     }
-//    finally
-//    {
-//      session.close();
-//    }
+    //We should not close the session(conversation) here, as we have more transactions coming later.
+  //  finally
+  //  {
+  //    session.close();
+  //  }
     
     System.out.println("Users are added to database");
     //Read all users
-//    session = HibernateSessionManager.getSessionFactory().openSession();
+  //  session = HibernateSessionManager.getSessionFactory().openSession();
     try {
       t = session.beginTransaction();
       //Using HQL (Hibernate Query Language) to read data from the database
@@ -57,8 +132,7 @@ public class Run {
     }
     finally {
       session.close();
-      HibernateSessionManager.shutdown();
-      System.out.println("Shut down");
-    }
+      System.out.println("session closed");
+    }    
   }
 }
